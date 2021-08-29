8bb1401a08adcf9602360013d4d89653dd00cb1c
AmitTalawar <80474676+AmitTalawar@users.noreply.github.com>
2021-08-29 16:07:53 +0530

    Update Quiz_1_Hackathon_1.cpp

--- Quiz_1_Hackathon_1.cpp
+++ Quiz_1_Hackathon_1.cpp
@@ -1,3 +1,12 @@
+/*
+Quiz-1-OOP
+Blood Donation Application - Weekend Hackathon
+The project was developed by :
+    Amit Appanna Talawar - 20BCS011
+    Rahul Pentamsetty - 20BCS106
+For the purpose of Weekend Hackathon organised by Dr. Uma S
+*/
+
 #include<bits/stdc++.h>
 
 using namespace std;
